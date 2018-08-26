7a741ffb052f85c41063d505595eb46295086265
Wilfredo Alcala <alcalawilfre@gmail.com>
2018-08-26 16:00:40 -0300

    Added conecction to MongoDB and functions to use mongoose as mongo client

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,33 +1,97 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling ./products'
-    });
+    Product.find().exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'List of available products',
+                products: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const createdProduct = req.body;
-    res.status(201).json({
-        message: 'Handling ./products with POST method',
-        createdProduct: createdProduct
-    });
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    })
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Handling ./products with POST method',
+                createdProduct: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Handling ./products with POST method',
+                error: err
+            });
+        });
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'ah1') {
-        res.status(200).json({
-            message: 'You have discovered the secret ID',
-            id: id
+    Product.findById(id).exec()
+        .then(doc => {
+            console.log(doc);
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
         });
-    } else {
-        res.status(200).json({
-            message: 'Wrong Id, try again',
-            id: id
-        });
+
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
     }
+    Product.update({ _id: id }, { $set: updateOps }).exec()
+    .then(result => {
+        res.status(200).json({
+            message: `The product was updated successfully`,
+            result: result
+        });
+    })
+    .catch( err => {
+        res.status(500).json({
+            message: `An error ocurred updating at ${id}`,
+            error: err
+        });
+    })
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({ _id: id }).exec()
+        .then(result => {
+            res.status(200).json({
+                message: `The ${id} was removed successfully`,
+                details: result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `An error ocurred removing at ${id}`,
+                error: err
+            });
+        });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const app = express();
+
+mongoose.connect(`mongodb+srv://alcalawil:${process.env.MONGO_ATLAS_PW}@alcalaw-cluster-13oup.mongodb.net/test?retryWrites=true`,
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Prevent CORS Errors
-app.use((req,res,next) =>{
-    res.header('Access-Control-Allow-Origin','*');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type- Accept, Authorization'
     );
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATH, GET')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATH, GET');
+        return res.status(200).json({});
     }
+    next();
 });
 
 //Routes that should be handled 
