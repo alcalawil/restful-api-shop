ba892b19a0b72b29bd9347d0118b5f429c82913f
Wilfredo Alcala <alcalawilfre@gmail.com>
2018-08-26 11:22:37 -0300

    modified to provent CORS Errors

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//Prevent CORS Errors
+app.use((req,res,next) =>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type- Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATH, GET')
+    }
+});
+
+//Routes that should be handled 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
